fa84929212bdd397c2232f0d836017a42c216ee2
Balmung421 <zcook421@gmail.com>
2017-07-24 09:09:37 -0600

    Added photograph class passing extists test

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,24 @@
+# > require './lib/photograph'
+# # => true
+#
+# > photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+# # => #<Photograph:0x007f98a9c6ace8 ...>
+#
+# > photograph.id
+# # => 1
+#
+# > photograph.artist_id
+# # => 4
+#
+# > photograph.museum_id
+# # => 2
+#
+# > photograph.name
+# # => "Rue Moffetard, Paris (Boy with Bottles)"
+#
+# > photograph.year
+# # => 1954
+
+
 class Photograph
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,9 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+
+  def test_it_exists
+    photo = Photograph.new
+    assert_instance_of Photograph, photo
+  end
 end
